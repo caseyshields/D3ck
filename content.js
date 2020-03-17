fafbaf3f75a274985c1c96a645b1f4615a733398
Casey <caseyorigami@gmail.com>
2020-03-16 20:55:23 -0700

    condensing tags

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ getContent = function () {
       title:"Western Dragon",
       type: "internal_link",
       summary:"One of my earliest origami designs. All fold angles are multiples of 22.5 degrees and relatively few folds are more than 2-ply. This makes it easier to retain accuracy and fold from a small square. The long limbs also make a variety of poses possible.",
-      tags: ['origami', 'dragon', 'tutorial'],
+      tags: ['origami', 'instructions', 'svg'],
       img:"img/dragon_CP.svg",
       link:"https://caseyshields.github.io/dragon/"
       // TODO add a list of dates to each entry...
@@ -47,14 +47,14 @@ getContent = function () {
       title: "Ladder Diagrams",
       type: "internal_link",
       summary: "Ladder diagrams are used to show messaging between actors over time, usually in protocols or software architectures. This example shows a streaming dataset and mouse interactivity.",
-      tags: ['D3', 'ladder', 'component'],
+      tags: ['D3', 'networking', 'component'],
       img: "img/ladder.svg",
       link: "https://caseyshields.github.io/ladder/"
     },{
       title: "Star Catalog",
       type: "internal_link",
       summary: "Made a starchart component in D3 to display some of the star catalogs used in sensor calibration. This example shows the first part of FK6. You can alter the projection by dragging and zooming. Click stars to display their catalog entries.",
-      tags: ['D3', 'astronomy', 'FK6', 'component'],
+      tags: ['D3', 'astronomy', 'component'],
       img: "img/stars.png",
       link: "https://caseyshields.github.io/starmap/"
     },{
@@ -110,14 +110,14 @@ getContent = function () {
       title: "Transforming A-10",
       type: "external_link", // TODO figure out how to embed youtube
       summary: "A Lego robot that transforms into an A10. Deceptively simple. Carefully designed to avoid 'shells' or 'backpack' mechanisms, minimised part number and cost, maximized the number of affordable color schemes, compromised between robot and vehicle proportions, while making it reasonably durable enough to actually play with!",
-      tags: ['lego', 'transformer', 'a10', 'thunderbolt'],
+      tags: ['lego', 'instructions', 'transformer'],
       img: "img/thunderbolt.png",
       link: "https://rebrickable.com/mocs/MOC-25799/deceptibot/transforming-a10-thunderbolt/"
     }, {
       title: "Origami Shadow Boxes",
       type: "external_link",
       summary: "Etsy store for my origami discoveries. Way too much work, and it's not worth any money, sadly. But still, I think these boxes really capture something; If a model is done 'right', you can't change the shape of an ear without changing every other bodily proportion. The whole geometry has a necessary balance. putting the final model against the crease pattern really gets this across I think.",
-      tags: ["origami", "art", "store"],
+      tags: ["origami", "art", "etsy"],
       img: "",
       link: "https://www.etsy.com/shop/CaseyOrigami"
     }, {
@@ -131,7 +131,7 @@ getContent = function () {
       title: "Orion Star Hunter",
       type: "external_link",
       summary: "A command line program that lets you search star catalogs, extrapolate thier position using Novas, then asynchronously direct sensors on a network at them. Thank goodness for the USNO. I still don't understand half the things they're calculating.",
-      tags: ["astronomy", "C", "Novas", "FK6"],
+      tags: ["astronomy", "C", "Novas"],
       img: "",
       link: "https://github.com/caseyshields/Orion"
     }
