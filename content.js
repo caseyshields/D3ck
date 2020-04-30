f7a22f646fb8100569219c6e2ec64230ab5519d5
Casey <caseyorigami@gmail.com>
2020-04-29 20:19:51 -0700

    added entry

--- content.js
+++ content.js
@@ -105,6 +105,13 @@ getContent = function () {
       tags: ["lego", "wargamming", "instructions"],
       img: "",
       link: "https://rebrickable.com/mocs/MOC-38895/deceptibot/knight-paladin/"
+    }, {
+      title: "Knight Gallant",
+      type: "external_link",
+      summary: "Yet another variation of my Imperial Knight Lego model. This one is a Questoris Gallant of House Griffith, equipped with a Stormspear Rocket pod and a light-up Paragon Gauntlet.",
+      tags: ["lego", "wargamming", "instructions"],
+      img: "",
+      link: "https://rebrickable.com/mocs/MOC-40607/deceptibot/knight-gallant/"
     }
   ]
 };
